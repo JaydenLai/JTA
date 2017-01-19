51873ad97242cc7ab773d09cd5060834b90f8512
Jayden Lai <jaydenli_hk@126.com>
2017-01-19 23:37:35 +0800

    First Commit

---
+++ jayden-test-app/src/test/java/com/hz/syxx/JaydenTestAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hz.syxx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JaydenTestAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
