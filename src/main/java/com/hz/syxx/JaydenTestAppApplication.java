549fdd55105ba96ed42988491e8eedab6241153c
Jayden Lai <jaydenli_hk@126.com>
2017-01-20 00:25:05 +0800

    reorganize

---
+++ src/main/java/com/hz/syxx/JaydenTestAppApplication.java
@@ -0,0 +1,12 @@
+package com.hz.syxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaydenTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaydenTestAppApplication.class, args);
+	}
+}
